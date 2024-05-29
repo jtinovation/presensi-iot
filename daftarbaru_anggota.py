7b3ede62d8bcddb226fa24bc22126405b3e3919f
Adam Darmawansa <adamdarmawansa439@gmail.com>
2024-05-29 13:43:13 +0700

    Adding new feature
    Menambahkan fitur display oled dan beberapa perubahan pada pesan yang ingin ditampilkan pada kondisi tertentu.

--- daftarbaru_anggota.py
+++ daftarbaru_anggota.py
@@ -5,6 +5,7 @@ import numpy as np
 import mysql.connector
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+import Adafruit_SSD1306
 
 # Nonaktifkan peringatan GPIO
 GPIO.setwarnings(False)
@@ -12,19 +13,29 @@ GPIO.setwarnings(False)
 # Inisialisasi pembaca RFID
 reader = SimpleMFRC522()
 
+# GPIO pin configuration for the OLED display
+OLED_RESET_PIN = None  # Set to GPIO pin if your display has a reset pin
+OLED = Adafruit_SSD1306.SSD1306_128_64(rst=OLED_RESET_PIN)
+
+# Initialize the OLED display
+OLED.begin()
+
 # Set the GPIO mode jika belum diatur
 try:
+
     GPIO.setmode(GPIO.BCM)
+
 except ValueError as e:
+    
     print(f"Mode GPIO sudah diatur: {e}")
 
 buzzer_pin = 11
-if GPIO.getmode() is None:
-    GPIO.setmode(GPIO.BCM)
-GPIO.setup(buzzer_pin, GPIO.OUT)
 
-# Membuat instance objek VideoCapture untuk webcam
-cap = cv2.VideoCapture(0)
+if GPIO.getmode() is None:
+
+    GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(buzzer_pin, GPIO.OUT)
 
 # Koneksi ke database MySQL
 db = mysql.connector.connect(
@@ -33,7 +44,12 @@ db = mysql.connector.connect(
     password="123",
     database="absensi"
 )
+
+# Cursor untuk menjalankan perintah SQL
 cursor = db.cursor()
+
+# Membuat instance objek VideoCapture untuk webcam
+cap = cv2.VideoCapture(0)
 
 def buzz(duration):
     # """Fungsi untuk menyalakan buzzer selama `duration` detik"""
@@ -42,39 +58,84 @@ def buzz(duration):
     GPIO.output(buzzer_pin, GPIO.LOW)
 
 def register_member():
+
     while True:
+        
         text = input("Nama anggota: ")
+
+        OLED.clear()
+        OLED.draw.text((0, 0), "Masukkan nama anggota!", font=None, fill=255)
+        OLED.display()
+        
         print("Silahkan scan RFID anda.")
+
+        OLED.clear()
+        OLED.draw.text((0, 0), "Silahkan RFID anda.", font=None, fill=255)
+        OLED.display()
+
         reader.write(text)
-        print("Scanning berhasil. Apakah data diri anda sebagai berikut?")
+
+        print("Scanning berhasil. Apakah data diri anda berikut ini sudah benar?")
+        
+        OLED.clear()
+        OLED.draw.text((0, 0), "Cek kembali data diri anda.", font=None, fill=255)
+        OLED.display()
+
         buzz(0.2)
+
         uid, read_text = reader.read()
         
         print(f"UID: {uid}")
         print(f"Nama: {read_text}")
         
-        validasi = input("Apakah data yang diinputkan sudah benar? (y/n): ")
+        validasi = input("Apakah data berikut sudah benar? (y/n): ")
+
+        OLED.clear()
+        OLED.draw.text((0, 0), "Apakah data berikut sudah benar?", font=None, fill=255)
+        OLED.display()
+
         if validasi.lower() == 'y':
+            
             buzz(0.2)
+            
             return uid, read_text
+        
         else:
+
             buzz(0.2)
+            
             print("Ulangi proses penulisan nama anggota RFID.\n")
 
 def create_dataset_and_db(uid, text):
+    
     folder_uid = str(uid)
     folder_name = text.replace(" ", " ")
     folder_path = os.path.join(os.getcwd(), "Datasets_User", folder_name)
 
     if os.path.exists(folder_path):
+        
         buzz(1.2)
+
         print("Gagal mendaftar, UID sudah terdaftar.")
+
+        OLED.clear()
+        OLED.draw.text((0, 0), "Gagal mendaftar.", font=None, fill=255)
+        OLED.display()
+
         return False
 
     cursor.execute(f"SHOW TABLES LIKE '{folder_name}'")
+    
     if cursor.fetchone():
+
         buzz(1.2)
+        
         print("Gagal mendaftar, UID sudah terdaftar.")
+        
+        OLED.clear()
+        OLED.draw.text((0, 0), "Gagal mendaftar.", font=None, fill=255)
+        OLED.display()
+
         return False
 
     os.makedirs(folder_path, exist_ok=True)
@@ -82,18 +143,28 @@ def create_dataset_and_db(uid, text):
 
     print(f"Folder {folder_name} telah dibuat.")
 
+    OLED.clear()
+    OLED.draw.text((0, 0), "Gagal mendaftar.", font=None, fill=255)
+    OLED.display()
+
     create_table_query = f"CREATE TABLE IF NOT EXISTS {folder_name} (id INT AUTO_INCREMENT PRIMARY KEY, \
                             nama VARCHAR(255), \
                             status_kehadiran VARCHAR(50), \
                             timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
     cursor.execute(create_table_query)
+    
     db.commit()
 
     print(f"Tabel {folder_name} telah dibuat di database.")
 
+    OLED.clear()
+    OLED.draw.text((0, 0), "Table data diri telah dibuat.", font=None, fill=255)
+    OLED.display()
+
     return folder_path, folder_uid
 
 def capture_images(folder_path):
+
     start_time = time.time()
     capture_duration = 20
     images = []
@@ -103,25 +174,42 @@ def capture_images(folder_path):
         ret, frame = cap.read()
         frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         file_path = os.path.join(folder_path, 'image', f"foto_{int(time.time())}.jpg")
+
         if not os.path.exists(file_path):
+
             cv2.imwrite(file_path, frameGray)
             images.append(frameGray)
 
     print("\n [INFO] Pengambilan foto selesai, melanjutkan ke proses training....")
+
+    OLED.clear()
+    OLED.draw.text((0, 0), "Pengambilan foto selesai, melanjutkan ke proses training....", font=None, fill=255)
+    OLED.display()    
+
     return images
 
 def train_recognizer(images, folder_uid):
+
     recognizer = cv2.face.LBPHFaceRecognizer_create()
     labels = [0] * len(images)
     recognizer.train(images, np.array(labels))
     trained_path = os.path.join(os.getcwd(), f"{folder_uid}.yml")
     recognizer.save(trained_path)
+    
     buzz(1.3)
+    
     print("\n [INFO] Histogram berhasil disimpan. Proses pendaftaran telah selesai.")
 
+    OLED.clear()
+    OLED.draw.text((0, 0), "Histogram berhasil disimpan. Proses pendaftaran telah selesai.", font=None, fill=255)
+    OLED.display() 
+
 def main():
+
     while True:
+
         try:
+
             uid, text = register_member()
             result = create_dataset_and_db(uid, text)
             
@@ -132,24 +220,51 @@ def main():
                 while True:
                     
                     start_capture = input("Apakah pengambilan foto bisa dimulai? (y/n): ")
+                    
+                    OLED.clear()
+                    OLED.draw.text((0, 0), "Apakah pengambilan foto bisa dimulai?", font=None, fill=255)
+                    OLED.display()
+
                     if start_capture.lower() == "y":
+
                         buzz(1.1)
+                        
                         images = capture_images(folder_path)
+                        
                         train_recognizer(images, folder_uid)
+                        
                         break
+
                     elif start_capture.lower() == "n":
+                        
                         buzz(0.2)
-                        print("\n [INFO] Menunggu 15 detik sebelum mencoba lagi....")
+
+                        print("\n [INFO] Menunggu 15 detik. Silahkan bersiap-siap")
+
+                        OLED.clear()
+                        OLED.draw.text((0, 0), "Menunggu 15 detik..", font=None, fill=255)
+                        OLED.display()
+
                         time.sleep(15)
+
                     else:
+
                         print("\n [INFO] Input tidak valid.")
 
+                        OLED.clear()
+                        OLED.draw.text((0, 0), "Input tidak valid", font=None, fill=255)
+                        OLED.display()
+
         except Exception as e:
+
             print(e)
+            
             break
 
     cap.release()
+
     GPIO.cleanup()
 
 if __name__ == "__main__":
+
     main()
--- presensi_anggota.py
+++ presensi_anggota.py
@@ -139,16 +139,37 @@ import time
 import mysql.connector
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+import Adafruit_SSD1306
 
-# Inisialisasi buzzer
-GPIO.setmode(GPIO.BCM)
+# Nonaktifkan peringatan GPIO
 GPIO.setwarnings(False)
-
-buzzer = 17
-GPIO.setup(buzzer, GPIO.OUT)
 
 # Inisialisasi pembaca RFID
 reader = SimpleMFRC522()
+
+# GPIO pin configuration for the OLED display
+OLED_RESET_PIN = None  # Set to GPIO pin if your display has a reset pin
+
+OLED = Adafruit_SSD1306.SSD1306_128_64(rst=OLED_RESET_PIN) #OLED specification configuration. 
+
+# Initialize the OLED display
+OLED.begin()
+
+# Set the GPIO mode jika belum diatur
+try:
+    GPIO.setmode(GPIO.BCM)
+
+except ValueError as e:
+    
+    print(f"Mode GPIO sudah diatur: {e}")
+
+buzzer_pin = 11
+
+if GPIO.getmode() is None:
+    
+    GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(buzzer_pin, GPIO.OUT)
 
 # Koneksi ke database MySQL
 db = mysql.connector.connect(
@@ -161,6 +182,9 @@ db = mysql.connector.connect(
 # Cursor untuk menjalankan perintah SQL
 cursor = db.cursor()
 
+# Create an instance of the VideoCapture object for webcam
+cap = cv2.VideoCapture(0)
+
 # Parameters
 font = cv2.FONT_HERSHEY_COMPLEX
 height = 1
@@ -169,29 +193,61 @@ nameColor = (255, 255, 255) # BGR- WHITE
 confColor = (255, 255, 0)   # BGR- YELLOW
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-# Create an instance of the VideoCapture object for webcam
-cap = cv2.VideoCapture(0)
+def buzz(duration):
+    
+    GPIO.output(buzzer_pin, GPIO.HIGH)
+
+    time.sleep(duration)
+    
+    GPIO.output(buzzer_pin, GPIO.LOW)
+
 
 def read_rfid():
-    print('Tempelkan RFID anda.')
+    
+    print('Tempelkan RFID anda!')
+
+    OLED.clear()
+    OLED.draw.text((0, 0), "Tempelkan RFID anda!", font=None, fill=255)
+    OLED.display()
+
     uid, text = reader.read()
+
+    buzz(1)
+
     folder_uid = str(uid)
     folder_name = text.replace(" ", " ")  # Ganti spasi dengan underscore
+    
     return folder_uid, folder_name
 
 def check_rfid_registered(folder_uid):
+
     histogram_path = os.path.join(os.getcwd(), f"{folder_uid}.yml")
+    
     if not os.path.exists(histogram_path):
+
         print("RFID belum terdaftar. Harap daftar terlebih dahulu.")
+
+        OLED.clear()
+        OLED.draw.text((0, 0), "RFID belum terdaftar. Harap daftar terlebih dahulu.", font=None, fill=255)
+        OLED.display()
+
+        buzz(1)
+        
         return False
+    
     recognizer.read(histogram_path)
+    
     return True
 
 def face_recognition(folder_name, start_time, cap):
+
     status_sent = False
+    
     while time.time() - start_time < 30:
+
         ret, frame = cap.read()
         frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_detector.detectMultiScale(
@@ -202,75 +258,135 @@ def face_recognition(folder_name, start_time, cap):
         )
 
         for (x, y, w, h) in faces:
+
             namepos = (x + 5, y - 5)
             confpos = (x + 5, y + h - 5)
             cv2.rectangle(frame, (x, y), (x + w, y + h), boxColor, 3)
             id, confidence = recognizer.predict(frameGray[y:y+h, x:x+w])
 
             if confidence >= 55 and not status_sent:
+                
                 label_name = "Wajah Dikenali"
                 confidence_text = f"{100 - confidence:.0f}%"
                 timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
                 send_to_database(folder_name, timestamp)
+                
                 status_sent = True
+                
                 break
+            
             else:
+                
                 label_name = "Wajah Tidak Dikenali"
                 confidence_text = f"{100 - confidence:.0f}%"
+                
+                buzz(1)
+
+                OLED.clear()
+                OLED.draw.text((0, 0), "Wjah tidak dikenali.", font=None, fill=255)
+                OLED.display()
 
             cv2.putText(frame, str(label_name), namepos, font, height, nameColor, 2)
+
             cv2.putText(frame, str(confidence_text), confpos, font, height, confColor, 1)
 
         cv2.imshow('Absensi', frame)
+        
         if status_sent:
+
             break
+
         if cv2.waitKey(20) & 0xFF == ord('q'):
+            
             break
 
     return status_sent
 
 def send_to_database(folder_name, timestamp):
+
     sql = f"INSERT INTO {folder_name} (nama, status_kehadiran, timestamp) VALUES (%s, %s, %s)"
     val = (folder_name, "hadir", timestamp)
     cursor.execute(sql, val)
     db.commit()
+    
     print("Status hadir telah dikirim ke database.")
 
+    OLED.clear()
+    OLED.draw.text((0, 0), "Status hadir telah dikirim ke database.", font=None, fill=255)
+    OLED.display()
+
+
 def main():
+
     while True:
+        
         try:
+
             folder_uid, folder_name = read_rfid()
+            
             if not check_rfid_registered(folder_uid):
+
                 continue
 
             print(f"Nama: {folder_name}")
             print("RFID terdaftar. Melanjutkan ke proses pengenalan wajah.")
+    
+            OLED.clear()
+            OLED.draw.text((0, 0), "RFID terdaftar. Melanjutkan ke proses pengenalan wajah.", font=None, fill=255)
+            OLED.display()
+
+            buzz(0.2)
 
             start_time = time.time()
             status_sent = face_recognition(folder_name, start_time, cap)
 
             if not status_sent:
+
                 cv2.destroyAllWindows()
+
                 print("Gagal mengenali wajah dalam 30 detik. Silakan tempelkan RFID kembali.")
+
+                OLED.clear()
+                OLED.draw.text((0, 0), "Wajah tidak dikenali.", font=None, fill=255)
+                OLED.display()
+                
+                buzz(1)
+                
                 continue  # Restart the process if no face is recognized
 
             if status_sent:
+
                 cv2.destroyAllWindows()
-                print("\nAbsensi telah selesai. Silakan tempelkan RFID kembali.")
+                
+                buzz(1)
+                
+                print("\nPresensi telah selesai. Silakan tempelkan RFID kembali.")
+
+                OLED.clear()
+                OLED.draw.text((0, 0), "Proses presensi berhasil.", font=None, fill=255)
+                OLED.display()
+                
                 continue  # Restart the process after successful attendance
 
         except Exception as e:
+
             print(e)
+            
             break
 
         key = cv2.waitKey(100) & 0xff
+        
         if key == 27 or key == 113:  # ESCAPE key or q key
+            
             break
 
     cap.release()
+
     GPIO.cleanup()
+    
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
+
     main()
 
